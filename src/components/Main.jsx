3fc1a72d0b27a08da202e0f5b6adda3066fa4cdb
Aravinda Rathnamalala <aravinda.rathnamalala@gmail.com>
2023-12-05 10:53:31 +0800

    dark mode impl

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,8 @@
 import { React } from "react";
 
-const Main = () => {
+const Main = (props) => {
   return (
-    <main>
+    <main className={props.darkMode ? "dark" : ""}>
       <h1 className="main--title">Fun Facts About React</h1>
       <ul className="main--facts"> 
         <li>Was first released in 2013</li>
